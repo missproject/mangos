48a6610415efe0b1e247078ca2e755e718ef4ba3
Thorazi <thorazi@gmx.com>
2009-04-23 19:53:05 +0200

    Fix new syntax for PlaySound, Replaced by PlaySoundDistance.
    Signed-off-by: Thorazi <thorazi@gmx.com>

--- src/game/mangchat/IRCCmde.cpp
+++ src/game/mangchat/IRCCmde.cpp
@@ -358,7 +358,7 @@ void IRCCmd::Fun_Player(_CDATA *CD)
         if(_PARAMS[1] == "sound")
         {
             uint32 sndid = atoi(_PARAMS[2].c_str());
-            plr->PlaySound(sndid ,true);
+            plr->PlayDistanceSound(sndid);
             Send_IRCA(ChanOrPM(CD), "\00313["+_PARAMS[0]+"] : Has Just Heard Sound ID: "+_PARAMS[2]+".", true, CD->TYPE);
         }
     }
@@ -1451,7 +1451,7 @@ void IRCCmd::PM_Player(_CDATA *CD)
             data << sMsg;
             data << (uint8)0;
             plr->GetSession()->SendPacket(&data);
-            plr->PlaySound(3081, true);
+            plr->PlayDistanceSound(3081);
             Send_IRCA(ChanOrPM(CD), "\00313To ["+_PARAMS[0]+"] : "+_PARAMS[1]+".", true, CD->TYPE);
         }
         else
